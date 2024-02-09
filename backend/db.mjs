e2dcb4abbe2516f3c0fd675feb099e9c9bd5df11
pakeg <pakegpakeg@gmail.com>
2024-02-09 03:01:33 +0200

    5 files modified

--- backend/db.mjs
+++ backend/db.mjs
@@ -2,6 +2,9 @@ import 'dotenv/config';
 import postgres from 'postgres';
 
 const connectionString = process.env.DATABASE_URL_POSTGRESQL;
-const sql = postgres(connectionString);
+const sql = postgres(connectionString, {
+  ssl: { rejectUnauthorized: false },
+  connect_timeout: 5,
+});
 
 export default sql;
