cdc93bf3288ccdefc4cc7f8fbe42bdedd5776dfa
pakeg <pakegpakeg@gmail.com>
2024-02-17 03:21:28 +0200

    Update server.mjs

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,8 +1,8 @@
-import express from 'express';
-import helmet from 'helmet';
-import compression from 'compression';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import helmet from "helmet";
+import compression from "compression";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 import {
   authorize,
@@ -14,7 +14,8 @@ import {
   getAllUser,
   getAllProductsInformation,
   updatePrice,
-} from './actions.mjs';
+  addParseLink,
+} from "./actions.mjs";
 
 const app = express();
 const port = 3000;
@@ -26,12 +27,12 @@ app.use(express.json());
 app.use(
   express.urlencoded({
     extended: true,
-  })
+  }),
 );
 const whitelist = [
   `https://lc6p8r-${port}.csb.app`,
   `https://lc6p8r-${client_port}.csb.app`,
-  'https://codesandbox.io',
+  "https://codesandbox.io",
   `http://localhost:${port}`,
   `http://localhost:${client_port}`,
 ];
@@ -40,103 +41,110 @@ const corsOptions = {
     if (whitelist.indexOf(origin) > -1 || !origin) {
       cb(null, true);
     } else {
-      cb(new Error('Запрещено CORS'));
+      cb(new Error("Запрещено CORS"));
     }
   },
   credentials: true,
-  methods: ['OPTIONS'],
+  methods: ["OPTIONS"],
 };
 app.use(cors(corsOptions));
 app.use(helmet());
 
 app.use(function (req, res, next) {
   if (
-    typeof req.cookies.authorized === 'undefined' &&
-    req.path != '/api/authorization'
+    typeof req.cookies.authorized === "undefined" &&
+    req.path != "/api/authorization"
   ) {
     res.status(401).json({
-      path: '/authorization',
-      msg: 'Not allowed',
-      statusText: 'Unauthorized',
+      path: "/authorization",
+      msg: "Not allowed",
+      statusText: "Unauthorized",
     });
   } else next();
 });
 
 // authorization
-app.get('/api/authorization', async function (req, res) {
-  if (typeof req.cookies.authorized !== 'undefined') {
+app.get("/api/authorization", async function (req, res) {
+  if (typeof req.cookies.authorized !== "undefined") {
     res.status(200).json({ authorized: req.cookies.authorized });
   } else res.status(200).json({ authorized: false });
 });
 
-app.post('/api/authorization', async function (req, res) {
+app.post("/api/authorization", async function (req, res) {
   const user = await authorize(req.body);
-  console.log(user, 'user - after');
+  console.log(user, "user - after");
   if (!user?.error) {
     if (user.redirect) {
       const options = {
         secure: true,
         httpOnly: false,
-        sameSite: 'None',
+        sameSite: "None",
         expires: +req.body.remember ? new Date(Date.now() + 60000) : 0,
       };
-      res.cookie('authorized', user.cookie, options);
+      res.cookie("authorized", user.cookie, options);
       res.status(201).json({ authorized: user.cookie });
     } else
       res.status(400).json({
-        path: '/authorization',
-        msg: 'Authorization failed',
-        statusText: 'Credentials wrong',
+        path: "/authorization",
+        msg: "Authorization failed",
+        statusText: "Credentials wrong",
       });
   } else res.status(500).send(user.error);
 });
 
 //------------
 // new product page
-app.post('/api/new-categoryitem', async function (req, res) {
+app.post("/api/new-categoryitem", async function (req, res) {
   const newItem = await createNewItemCategory(req.body);
   if (!newItem?.error) res.status(201).json(newItem);
   else res.status(400).send(newItem.error);
 });
 
-app.post('/api/new-product', async function (req, res) {
+app.post("/api/new-product", async function (req, res) {
   const isNewProduct = await createNewProduct(req.body);
   if (!isNewProduct?.error) res.status(201).json({ created: true });
   else res.status(400).send(isNewProduct.error);
 });
 
-app.post('/api/getcategories-product', async function (req, res) {
+app.post("/api/getcategories-product", async function (req, res) {
   const items = await getCategoriesProduct();
   if (items) res.status(201).json(items);
   else res.sendStatus(500);
 });
 //-----------------
 // AdminPanel
-app.post('/api/create-new-user', async function (req, res) {
+app.post("/api/create-new-user", async function (req, res) {
   const user = await createNewUser(req.body);
   res.status(201).json(user);
 });
 
-app.post('/api/update-user', async function (req, res) {
+app.post("/api/update-user", async function (req, res) {
   const updatedUser = await updateUserData(req.body);
   res.status(201).json(updatedUser);
 });
 
-app.post('/api/getall-user', async function (req, res) {
+app.post("/api/getall-user", async function (req, res) {
   const users = await getAllUser();
   res.status(201).json(users);
 });
 //---------------------
 //------main page-----------
-app.post('/api/get-all-information', async function (req, res) {
+app.post("/api/get-all-information", async function (req, res) {
   const data = await getAllProductsInformation();
   if (!data?.error) {
     res.status(201).json(data);
   } else res.status(500).send(data.error);
 });
 
-app.post('/api/post-update-price', async function (req, res) {
+app.post("/api/post-update-price", async function (req, res) {
   const data = await updatePrice(req.body);
+  if (!data?.error) {
+    res.status(201).json(data);
+  } else res.status(500).send(data.error);
+});
+
+app.post("/api/post-add-parse-link", async function (req, res) {
+  const data = await addParseLink(req.body);
   if (!data?.error) {
     res.status(201).json(data);
   } else res.status(500).send(data.error);
