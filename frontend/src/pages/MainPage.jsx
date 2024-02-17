7bcdf7b3263e5641120b7d6a99794a2a2464f75b
pakeg <pakegpakeg@gmail.com>
2024-02-17 03:31:21 +0200

    Update MainPage.jsx

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,138 +1,101 @@
-import { buildCreateSlice, asyncThunkCreator } from "@reduxjs/toolkit";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { memoize } from "proxy-memoize";
 
-import {
-  fetchAllInformation,
-  updatePrice,
-  addParseLink,
-} from "../actions/actionsMainPage";
-import { fetchDataCategories } from "./newProductSlice";
-import { sortReducer } from "../actions/createdActions";
+import { fetchGeneralData } from "../store/reducers/mainPageSlice.js";
+import { getSortedDataSelector } from "../store/actions/createdActions.js";
 
-const initialState = {
-  loading: false,
-  errors: null,
-  data: {},
-  revalidate: null,
-  sort: [],
-  search: { title: "", part_number: "" },
-  filters: { category: [], manufacture: [] },
+import PartMainOne from "../components/PartMainOne.jsx";
+import PartMainTwo from "../components/PartMainTwo.jsx";
+import PartMainThree from "../components/PartMainThree.jsx";
+import ModalDelete from "../components/Modals/ModalDelete.jsx";
+import ModalAddShop from "../components/Modals/ModalAddShop.jsx";
+import ModalComments from "../components/Modals/ModalComments.jsx";
+
+import useScroll from "../hooks/useScroll.jsx";
+import useScrollHorizontal from "../hooks/useScrollHorizontal";
+
+export const MainPage = () => {
+  const { loading, errors, data } = useSelector(
+    memoize((state) => ({
+      ...state.mainPageReducer,
+      data: {
+        ...getSortedDataSelector(state.mainPageReducer, "products"),
+        ...state.newProductReducer.data,
+      },
+    })),
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (typeof data.products === "undefined") dispatch(fetchGeneralData());
+  }, []);
+
+  const { isScroll, boxScroll, buttonScroll } = useScroll(true);
+  const {
+    isScroll: isScrollHor,
+    boxScroll: boxScrollHor,
+    buttonScroll: buttonScrollHor,
+  } = useScrollHorizontal();
+
+  return (
+    <div>
+      <div className="flex">
+        <div className="overflow-hidden">
+          <div
+            ref={boxScroll}
+            className={`max-h-[300px] overflow-y-auto overflow-x-hidden ${
+              isScroll && "-mr-[17px]"
+            }`}
+          >
+            <div className="flex">
+              {/* -------Table N. 1--------- */}
+              <PartMainOne
+                products={data.products}
+                categories={data.categories}
+                manufactures={data.manufactures}
+              />
+              {/* -------Table N. 2--------- */}
+              <PartMainTwo
+                shops={data.shops}
+                shopsTableRows={data.shopsTableRows}
+                boxScrollHor={boxScrollHor}
+              />
+              {/* -------Table N. 2--------- */}
+              <PartMainThree products={data.products} />
+              {/*----------------------------*/}
+            </div>
+          </div>
+        </div>
+        <div className="bg-[#c1c1c1] rounded-3xl shadow-[inset_0px_0px_1px_0px_black]">
+          <div
+            className={` bg-[#e1e1e1] rounded-3xl cursor-grabbing ${
+              !isScroll ? "opacity-0 invisible" : "w-3.5 opacity-100 visible"
+            }`}
+            ref={buttonScroll}
+          ></div>
+        </div>
+
+        <div className="bg-[#c1c1c1] rounded-3xl shadow-[inset_0px_0px_1px_0px_black] absolute h-max">
+          <div
+            className={` bg-[#e1e1e1] rounded-3xl cursor-grabbing ${
+              !isScrollHor ? "opacity-0 invisible" : "h-3.5 opacity-100 visible"
+            }`}
+            ref={buttonScrollHor}
+          ></div>
+        </div>
+      </div>
+      <ModalDelete />
+      {/* <ModalAddShop stores={[...products.stores]} /> */}
+
+      {/* {
+            id: 0,
+            date: 1698084416000,
+            price: 134,
+            comment:
+              "Lorem Ipsum is simply dummy",
+          }, */}
+      <ModalComments productComments={null} />
+    </div>
+  );
 };
-const createSliceWithThunks = buildCreateSlice({
-  creators: { asyncThunk: asyncThunkCreator },
-});
-
-const mainPageSlice = createSliceWithThunks({
-  name: "mainPage",
-  initialState,
-  reducers: (create) => ({
-    fetchGeneralData: create.asyncThunk(
-      async (_, { signal, getState, dispatch }) => {
-        const {
-          mainPageReducer: { revalidate },
-          newProductReducer: { data: categories },
-        } = getState();
-        if (Object.keys(categories).length === 0) {
-          dispatch(fetchDataCategories());
-        }
-        if (revalidate == null || revalidate < Date.now()) {
-          const data = await fetchAllInformation(signal);
-          return data;
-        }
-        return {};
-      },
-      {
-        pending: (state) => {
-          state.loading = true;
-        },
-        rejected: (state, action) => {
-          state.loading = false;
-        },
-        fulfilled: (state, action) => {
-          state.loading = false;
-          state.revalidate = Date.now() + 60 * 60 * 1000;
-          state.data = action.payload;
-        },
-      },
-    ),
-    fetchUpdatePrice: create.asyncThunk(
-      async ({ id, price }, { signal }) => {
-        const data = await updatePrice({ id, price }, signal);
-        return data;
-      },
-      {
-        pending: (state) => {
-          state.loading = true;
-        },
-        rejected: (state, action) => {
-          state.loading = false;
-        },
-        fulfilled: (state, action) => {
-          state.loading = false;
-          const findedProductsIndex = state.data.products.findIndex(
-            (product) => product.id === action.payload.id,
-          );
-
-          const row = [];
-          state.data.shops.forEach((shop) => {
-            if (shop.active_status != "0") {
-              const finded = state.data.products[
-                findedProductsIndex
-              ].shops_data.find((shop_data) => shop_data.shop.id === shop.id);
-              if (finded) {
-                finded.price = action.payload.price;
-              }
-              row.push(finded);
-            }
-          });
-          state.data.shopsTableRows[findedProductsIndex] = row;
-          state.data.products[findedProductsIndex].price = action.payload.price;
-        },
-      },
-    ),
-    fetchAddParseLink: create.asyncThunk(
-      async (data, { signal }) => {
-        const res = await addParseLink(data, signal);
-        return res;
-      },
-      {
-        pending: (state) => {
-          state.loading = true;
-        },
-        rejected: (state, action) => {
-          state.loading = false;
-        },
-        fulfilled: (state, action) => {
-          console.log(action.payload, "action.payload");
-        },
-      },
-    ),
-    setSort: sortReducer(create),
-    setFiltersReducer: create.reducer(
-      (state, { payload: { properties, filters } }) => {
-        if (filters.length === 0) {
-          state.filters[properties] = [];
-          return;
-        }
-        state.filters = { ...state.filters, [properties]: filters };
-      },
-    ),
-    setSearchReducer: create.reducer(
-      (state, { payload: { properties, querySearch } }) => {
-        if (querySearch.length === 0) {
-          state.search[properties] = "";
-          return;
-        }
-        state.search = { ...state.search, [properties]: querySearch };
-      },
-    ),
-  }),
-});
-
-export const {
-  fetchGeneralData,
-  fetchUpdatePrice,
-  fetchAddParseLink,
-  setFiltersReducer,
-  setSearchReducer,
-} = mainPageSlice.actions;
-export default mainPageSlice.reducer;
